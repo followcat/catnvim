a161efd48e01f908bab313191f41816843415fb7
followcat <followcat@gmail.com>
2025-11-07 15:51:39 +0800

    fix: a conflict between the old archived null-ls.nvim and the new maintained none-ls.nvim fork;

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -89,7 +89,7 @@ return {
 		event = { "BufReadPre", "BufNewFile" },
 		dependencies = {
 			"williamboman/mason.nvim",
-			"jose-elias-alvarez/null-ls.nvim",
+			"nvimtools/none-ls.nvim",
 		},
 		config = function()
 			require("mason-null-ls").setup({
